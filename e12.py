aecbdef034e6fb23ffef18633eb07ec6a9f90238
joaquin1092 <pepelotorres614@gmai.com>
2026-05-05 09:06:34 -0300

    Añado ejercicio e12.py

---
+++ e12.py
@@ -0,0 +1,6 @@
+# Los estudiantes deben pasar al frente a exponer, pero nadie quiere pasar primero.
+# Diseñar un programa con una lista con los nombres de los estudiantes, y de forma aleatoria
+# generar otra lista que indique el orden en el que deben pasar a exponer.
+
+# Ayuda: Usar bucle for i range(len(estudiantes)):
+# Ayuda: Para añadir un elemento a una lista se puede usar el método append().
