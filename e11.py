9a610735bc970de242a8409f73473531aa4a5915
Rivadero León <leonrivaderoz@gmail.com>
2026-05-04 22:36:54 -0300

    Agrego comentarios a la correción

--- e11.py
+++ e11.py
@@ -3,8 +3,8 @@ def procesar_inventario():
     stock = [15, 8, 20]
     precios = [25000, 5000, 3500]
     
-    ganancias_totales = 0
-    ventas_totales = 0
+    ganancias_totales = 0 #agrego la variable ganancias_totales para acumular la ganancia de cada operación
+    ventas_totales = 0 
     pedidos_pendientes = [10, 12, 5] 
 
     print("--- Sistema de Despacho de Almacén ---")
@@ -12,15 +12,15 @@ def procesar_inventario():
     for i in range(len(productos)):
         nombre = productos[i]
         cantidad_pedida = pedidos_pendientes[i]
-        stock_actual = stock[i]
+        stock_actual = stock[i] 
         precio_unitario = precios[i]
 
         print(f"Analizando pedido de: {nombre}")
 
-        if stock_actual > cantidad_pedida: #cambie el operador por '>', haciendo que si el stock acyual es mayor que la cantidad pedida, se procese el pedido
+        if stock_actual > cantidad_pedida: #cambie el operador por '>', haciendo que si el stock actual es mayor que la cantidad pedida, se procese el pedido
             total_operacion = cantidad_pedida * precio_unitario
-            ganancias_totales += total_operacion
-            ventas_totales += cantidad_pedida
+            ganancias_totales += total_operacion #acumulo la ganancia de la operación a la variable ganancias_totales
+            ventas_totales += cantidad_pedida #acumulo la cantidad vendida a la variable ventas_totales
             stock[i] = stock_actual - cantidad_pedida
             print(f"Pedido procesado. Stock restante: {stock[i]}")
         else:
