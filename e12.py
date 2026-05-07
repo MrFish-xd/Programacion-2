4cc6a2bbb3ea20a463479b306bf966427ff156c3
Rivadero León <leonrivaderoz@gmail.com>
2026-05-07 00:10:38 -0300

    Agrego ejercicio e12.py resuelto

--- e12.py
+++ e12.py
@@ -4,3 +4,15 @@
 
 # Ayuda: Usar bucle for i range(len(estudiantes)):
 # Ayuda: Para añadir un elemento a una lista se puede usar el método append().
+# Solo se puede usar random.randint() para generar un número aleatorio entre 0 y el tamaño de la lista de estudiantes, y se puede usar el for.
+
+import random
+estudiantes = ["Federico", "Hana", "Leonardo", "Ismael", "Danilo", "Valentin", "Mateo", "León", "Santiago", "Lucia"]
+orden_exposicion = []
+for i in range(len(estudiantes)):
+    aleatorio = random.randint(0, len(estudiantes) - 1)
+    orden_exposicion.append(estudiantes.pop(aleatorio))
+    
+print("El orden de exposición es:")
+for i in range(len(orden_exposicion)):
+    print(f"{i + 1}. {orden_exposicion[i]}")
